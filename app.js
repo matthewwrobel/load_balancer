628c5d3f4be0ac9f7173a01a9a235eb986a5f041
Matthew Wrobel <matthew.wrobel@gmail.com>
2021-08-16 21:04:38 -0700

    implemented basic load balancer

--- app.js
+++ app.js
@@ -4,29 +4,16 @@ const port = 1000;
 require('dotenv').config();
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
-console.log(process.env.SERVERS);
 let servers = (process.env.SERVERS).split(' ');
-console.log(servers);
 
 let index = 0;
 
 app.use('/', (req, res, next) => {
   index === 3 ? index = 0 : index++;
-  console.log(index);
   next();
 });
 
 app.use('/', createProxyMiddleware({ target: `http://${servers[index]}:8000`, changeOrigin: true}));
-
-
-
-
-
-// app.get('/', (req, res) => {
-//   res.send('Hello World');
-// });
-
-
 
 app.listen(port, () => {
   console.log(`Load balancer running at http://localhost:${port}`)
