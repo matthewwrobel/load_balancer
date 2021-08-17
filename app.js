4c6d92bc339e8add5f9c2ee7acdeb9c1a09ae0b2
Matthew Wrobel <matthew.wrobel@gmail.com>
2021-08-16 20:23:41 -0700

    added http proxy

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
+const express = require('express');
+const app = express();
+const port = 1000;
+require('dotenv').config();
 
+const { createProxyMiddleware } = require('http-proxy-middleware');
+app.use('/', createProxyMiddleware({ target: 'http://18.218.110.105:8000', changeOrigin: true}));
+
+// console.log(process.env.SERVERS);
+// let servers = (process.env.SERVERS).split(' ');
+// console.log(servers);
+
+
+
+// app.get('/', (req, res) => {
+//   res.send('Hello World');
+// });
+
+
+
+app.listen(port, () => {
+  console.log(`Load balancer running at http://localhost:${port}`)
+})
