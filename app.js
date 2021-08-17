f94ffd1a579ddb68f521308998e41d6284967f61
Matthew Wrobel <matthew.wrobel@gmail.com>
2021-08-16 20:41:40 -0700

    started round robin logic

--- app.js
+++ app.js
@@ -2,13 +2,23 @@ const express = require('express');
 const app = express();
 const port = 1000;
 require('dotenv').config();
-
 const { createProxyMiddleware } = require('http-proxy-middleware');
-app.use('/', createProxyMiddleware({ target: 'http://18.218.110.105:8000', changeOrigin: true}));
 
-// console.log(process.env.SERVERS);
-// let servers = (process.env.SERVERS).split(' ');
-// console.log(servers);
+console.log(process.env.SERVERS);
+let servers = (process.env.SERVERS).split(' ');
+console.log(servers);
+
+let index = 0;
+
+app.use('/', (req, res, next) => {
+  index === 3 ? index = 0 : index++;
+  console.log(index);
+  next();
+});
+
+app.use('/', createProxyMiddleware({ target: `http://${servers[index]}:8000`, changeOrigin: true}));
+
+
 
 
 
