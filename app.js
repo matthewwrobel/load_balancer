fc64bb4addd4a911bc76ae9fcd5229c052d402cc
Matthew Wrobel <matthew.wrobel@gmail.com>
2021-08-17 20:34:55 -0700

    refactored logic

--- app.js
+++ app.js
@@ -4,17 +4,40 @@ const port = 1000;
 require('dotenv').config();
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
-let servers = (process.env.SERVERS).split(' ');
+const servers = (process.env.SERVERS).split(' ');
+const server1 = createProxyMiddleware({ target: `http://${servers[0]}:8000`, changeOrigin: true});
+const server2 = createProxyMiddleware({ target: `http://${servers[1]}:8000`, changeOrigin: true});
+const server3 = createProxyMiddleware({ target: `http://${servers[2]}:8000`, changeOrigin: true});
+const server4 = createProxyMiddleware({ target: `http://${servers[3]}:8000`, changeOrigin: true});
 
 let index = 0;
 
 app.use('/', (req, res, next) => {
-  index === 3 ? index = 0 : index++;
+  index ===  servers.length - 1 ? index = 0 : index++;
   next();
 });
 
-app.use('/', createProxyMiddleware({ target: `http://${servers[index]}:8000`, changeOrigin: true}));
+app.use('/', (req, res, next) => {
+
+  if (index === 0) {
+    console.log(index);
+    server1(req, res, next);
+  }
+  if (index === 1) {
+    console.log(index);
+    server2(req, res, next);
+  }
+  if (index === 2) {
+    console.log(index);
+    server3(req, res, next);
+  }
+  if (index === 3) {
+    console.log(index);
+    server4(req, res, next);
+  }
+
+});
 
 app.listen(port, () => {
   console.log(`Load balancer running at http://localhost:${port}`)
-})
+});
